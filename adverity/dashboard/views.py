61e59ae5cb88ea4774e89eff60a1734df1f70ae4
Mathew Dragon <mdragon@localhost.localdomain>
2020-02-24 10:13:01 +0100

    clenup

--- adverity/dashboard/views.py
+++ adverity/dashboard/views.py
@@ -13,20 +13,17 @@ class IndexView(FormView):
     template_name = "dashboard/index.html"
     form_class = FiltersForm
 
-    def form_valid(self, form):
-        print("form is valid")
-        return super().form_valid(form)
 
     def get_initial(self):
+        """ initialize form with data from GET request """
         initial = super().get_initial()
         initial.update(dict(self.request.GET))
 
         return initial
 
     def get_context_data(self, **kwargs):
-        print(self.request.GET)
-        print(dir(self))
+        """ return context for the view """
+
         context = super().get_context_data()
         context.update(dict(self.request.GET))
-        print(context)
         return context
